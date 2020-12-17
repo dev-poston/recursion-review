d7a0aff1e635a0a0f611165564bae1d428eb11bf
Devon Poston <dev.poston@gmail.com>
2020-12-17 13:08:06 -0800

    complete jsonstringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,41 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === 'number') {
+    return obj.toString();
+  }
+  if (obj === null) {
+    return 'null';
+  }
+  if (typeof obj === 'boolean') {
+    return obj.toString();
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var arrResult = '[';
+    for (var i = 0; i < obj.length; i++) {
+      if (obj[i] === obj[obj.length - 1]) {
+        arrResult += stringifyJSON(obj[i]);
+      } else {
+        arrResult += stringifyJSON(obj[i]) + ',';
+      }
+    }
+    return arrResult + ']';
+  }
+  if (typeof obj === 'object') {
+    if (_.isEmpty(obj)) {
+      return '{}';
+    }
+    var objResult = '{';
+    for (var key in obj) {
+      if (obj[key] === 'function' || obj[key] === undefined) {
+        return '{}';
+      }
+      objResult += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+    }
+    var sliced = objResult.slice(0, -1);
+    return sliced + '}';
+  }
 };
